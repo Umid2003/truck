4b2f48c0d678ac733000d14379c576b3edfbca78
Umid2003 <karl_karl_2020@inbox.ru>
2025-05-08 14:15:21 +0500

    added

---
+++ src/data.js
@@ -0,0 +1,16 @@
+import Labo from './assets/labo.png'
+import Changan from './assets/changan.png'
+import Bongo from './assets/bongo.png'
+
+export const trucks = [
+    { model: 'Labo', number: '60 A 101 AA', phone: '+998 94 101 01 01', driver: 'Oybek', maxWeight: '2 tonna', color: 'white', img: Labo },
+    { model: 'Changan', number: '60 A 202 BB', phone: '+998 93 114 12 22', driver: 'Qodirjon', color: 'silver', maxWeight: '3.5 tonna', img: Changan },
+    { model: 'Bongo', number: '60 B 301 AC', phone: '+998 90 225 14 15', driver: 'Sodiqjon', color: 'blue', maxWeight: '4.5 tonna', img: Bongo },
+    { model: 'Labo', number: '60 A 101 AA', phone: '+998 94 101 01 01', driver: 'Oybek', maxWeight: '2 tonna', color: 'white', img: Labo },
+    { model: 'Changan', number: '60 A 202 BB', phone: '+998 93 114 12 22', driver: 'Qodirjon', color: 'silver', maxWeight: '3.5 tonna', img: Changan },
+    { model: 'Bongo', number: '60 B 301 AC', phone: '+998 90 225 14 15', driver: 'Sodiqjon', color: 'blue', maxWeight: '4.5 tonna', img: Bongo },
+    { model: 'Labo', number: '60 A 101 AA', phone: '+998 94 101 01 01', driver: 'Oybek', maxWeight: '2 tonna', color: 'white', img: Labo },
+    { model: 'Changan', number: '60 A 202 BB', phone: '+998 93 114 12 22', driver: 'Qodirjon', color: 'silver', maxWeight: '3.5 tonna', img: Changan },
+    { model: 'Bongo', number: '60 B 301 AC', phone: '+998 90 225 14 15', driver: 'Sodiqjon', color: 'blue', maxWeight: '4.5 tonna', img: Bongo },
+
+]
